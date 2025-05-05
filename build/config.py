79119c254f754d98d0863139821bb149b45d6fef
Jenggot2008 <razan.ihsan468@gmail.com>
2025-05-05 13:15:22 +0700

    Delete build directory

--- build/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# API Key Gemini
-GOOGLE_API_KEY = "AIzaSyAjtK5VMleflxeMmJsRRZHVgGK2tJPKGDA"
-os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
-
-# Path dan direktori
-DB_PATH = "database.db"
-PERSIST_DIRECTORY = "./chroma_langchain_newest"
